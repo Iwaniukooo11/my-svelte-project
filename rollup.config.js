17cbeaf72fd412d1f544e6eda6a2d5c5a530163c
Iwaniukooo11 <mateusz.iwaniuk11@gmail.com>
2020-03-23 18:14:10 +0100

    add coments in rollup

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import { terser } from "rollup-plugin-terser";
 
 const production = !process.env.ROLLUP_WATCH;
 
-import autoPreprocess from "svelte-preprocess";
+import autoPreprocess from "svelte-preprocess"; //<-- ADD THIS LINE!
 
 export default {
   input: "src/main.js",
@@ -18,7 +18,7 @@ export default {
   },
   plugins: [
     svelte({
-      preprocess: autoPreprocess(),
+      preprocess: autoPreprocess(), //<-- ADD THIS LINE!
       // enable run-time checks when not in production
       dev: !production,
       // we'll extract any component CSS out into
